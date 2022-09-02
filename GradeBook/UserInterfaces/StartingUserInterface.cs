0bdca8391962346ba9fa2df37f340cdf2abd1943
Helen Hua <wenhua@microsoft.com>
2022-09-02 21:31:12 +0800

    update codes

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,11 +40,11 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            var type = parts[2].ToLower();
+            var type = parts[2];
             BaseGradeBook gradeBook;
-            if (type == "standard") 
+            if (type == "standard")
                 gradeBook = new StandardGradeBook(name);
-            else if (type == "ranked") 
+            if (type == "ranked")
                 gradeBook = new RankedGradeBook(name);
             else
             {
